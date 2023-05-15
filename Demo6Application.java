b1fd307b9dbde58f11d7f2e82c1f29da4632a536
MayarSultanAlanaze <126346292+MayarSultanAlanaze@users.noreply.github.com>
2023-05-15 12:09:03 +0300

    Add files via upload

---
+++ Demo6Application.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo6Application.class, args);
+    }
+
+     /* output1
+     hey from message1
+     */
+
+
+    /* output 2
+      hey from message1
+        hey from message2
+     */
+
+
+    /* output 3
+      hey from message1
+        hey from message3
+        hey from message2
+     */
+
+
+      /* output 4
+      hey from message1
+        hey from message3
+        hey from message2
+        hey from main controller
+     */
+
+
+    /* output 5
+      hey from message1
+        hey from message3
+        hey from message2
+     */
+
+
+}
